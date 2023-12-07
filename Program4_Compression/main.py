7d861b5ed9e3a62cd2081808347d91998c98b1f2
RayyanZaid <rayyanzaid0401@gmail.com>
2023-12-06 19:00:20 -0800

    fixed

--- Program4_Compression/main.py
+++ Program4_Compression/main.py
@@ -1,88 +1,123 @@
 import heapq
-from collections import Counter
+import os
+import json
+import bitarray
 
-def build_huffman_tree(text):
-    """
-    Build the Huffman tree for the given text.
-    Returns the root of the tree and the dictionary with character encodings.
-    """
-    frequency = Counter(text)
-    heap = [[weight, [symbol, ""]] for symbol, weight in frequency.items()]
-    heapq.heapify(heap)
 
-    while len(heap) > 1:
-        lo = heapq.heappop(heap)
-        hi = heapq.heappop(heap)
-        for pair in lo[1:]:
-            pair[1] = '0' + pair[1]
-        for pair in hi[1:]:
-            pair[1] = '1' + pair[1]
-        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    
-    tree = sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
-    huffman_code = {symbol: code for symbol, code in tree}
-    return huffman_code
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
 
-def compress_huffman(text, huffman_code):
-    """
-    Compress the text using the Huffman coding.
-    Returns the compressed binary string.
-    """
-    return ''.join(huffman_code[char] for char in text)
+    def __lt__(self, other):
+        return self.freq < other.freq
 
-def decompress_huffman(compressed_text, huffman_code):
-    """
-    Decompress the compressed text using the Huffman coding.
-    Returns the original text.
-    """
-    reverse_huffman_code = {v: k for k, v in huffman_code.items()}
-    decoded_text = ''
-    current_code = ''
-    for bit in compressed_text:
-        current_code += bit
-        if current_code in reverse_huffman_code:
-            decoded_text += reverse_huffman_code[current_code]
-            current_code = ''
+
+def make_frequency_dict(text):
+    frequency = {}
+    for char in text:
+        if not char in frequency:
+            frequency[char] = 0
+        frequency[char] += 1
+    return frequency
+
+
+def make_huffman_tree(frequency):
+    priority_queue = [Node(char, freq) for char, freq in frequency.items()]
+    heapq.heapify(priority_queue)
+
+    while len(priority_queue) > 1:
+        left = heapq.heappop(priority_queue)
+        right = heapq.heappop(priority_queue)
+
+        merged = Node(None, left.freq + right.freq)
+        merged.left = left
+        merged.right = right
+
+        heapq.heappush(priority_queue, merged)
+
+    return priority_queue[0]
+
+
+def make_codes(root, current_code, codes):
+    if root is None:
+        return
+
+    if root.char is not None:
+        codes[root.char] = current_code
+
+    make_codes(root.left, current_code + "0", codes)
+    make_codes(root.right, current_code + "1", codes)
+
+
+def huffman_encoding(text):
+    frequency = make_frequency_dict(text)
+    root = make_huffman_tree(frequency)
+
+    codes = {}
+    make_codes(root, "", codes)
+
+    encoded_text = ''.join([codes[char] for char in text])
+    return encoded_text, codes
+
+
+def huffman_decoding(encoded_text, codes):
+    reverse_codes = {v: k for k, v in codes.items()}
+    decoded_text = ""
+    code = ""
+
+    for bit in encoded_text:
+        code += bit
+        if code in reverse_codes:
+            decoded_text += reverse_codes[code]
+            code = ""
+
     return decoded_text
 
-# Example usage
-def read_text_from_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            return file.read()
-    except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-        return None
 
-def write_text_to_file(file_path, text):
-    with open(file_path, 'w') as file:
-        file.write(text)
+def compress(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
 
-# Read text from a file
-file_path = "input.txt"  # Replace with the path to your text file
-text = read_text_from_file(file_path)
+    encoded_text, codes = huffman_encoding(text)
+    b = bitarray.bitarray(encoded_text)
 
-if text is not None:
-    # Perform Huffman coding operations on the text
-    huffman_code = build_huffman_tree(text)
-    compressed_text = compress_huffman(text, huffman_code)
-    decompressed_text = decompress_huffman(compressed_text, huffman_code)
+    compressed_file_path = file_path + '.bin'
+    with open(compressed_file_path, 'wb') as file:
+        b.tofile(file)
 
-    # Print and save the results
-    print("Original Text:", text)
-    print("Compressed Text:", compressed_text)
-    print("Decompressed Text:", decompressed_text)
-    print("Huffman Code:", huffman_code)
+    code_file_path = file_path + '.codes'
+    with open(code_file_path, 'w', encoding='utf-8') as file:
+        json.dump(codes, file)
 
-    # Save the compressed text to a file if needed
-    write_text_to_file("compressed_text.txt", compressed_text)
-else:
-    print("Unable to proceed due to missing input text.")
+    print("Compressed file created:", compressed_file_path)
+    print("Codes file created:", code_file_path)
+
+
+def decompress(compressed_file_path, code_file_path):
+    with open(code_file_path, 'r', encoding='utf-8') as file:
+        codes = json.load(file)
+
+    b = bitarray.bitarray()
+    with open(compressed_file_path, 'rb') as file:
+        b.fromfile(file)
+    encoded_text = b.to01()
+
+    decoded_text = huffman_decoding(encoded_text, codes)
+
+    decompressed_file_path = compressed_file_path.replace(
+        '.bin', '_decompressed.txt')
+    with open(decompressed_file_path, 'w', encoding='utf-8') as file:
+        file.write(decoded_text)
+
+    print("Decompressed file created:", decompressed_file_path)
 
 
 def count_bits_in_text_file(file_path):
     try:
-        # Open the file in binary mode and read its content
+
         with open(file_path, 'rb') as file:
             content = file.read()
 
@@ -95,8 +130,15 @@ def count_bits_in_text_file(file_path):
     except Exception as e:
         print(f'An error occurred: {str(e)}')
 
-# Example usage:
-file_path = 'input.txt'
-count_bits_in_text_file(file_path)
-file_path = 'compressed.txt'
-count_bits_in_text_file(file_path)
+
+def test(inputFileName):
+
+    compress(f'{inputFileName}')
+    decompress(f'{inputFileName}.bin', f'{inputFileName}.codes')
+
+    count_bits_in_text_file(f"{inputFileName}")
+    count_bits_in_text_file(f"{inputFileName}.bin")
+
+
+# don't
+test("input.txt")
